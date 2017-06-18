a87d191c6d2c296035846789a3e0ee84eeebe402
Vishwanath <project.initial18@gmail.com>
2017-06-18 22:46:57 +0530

    First cut of configuration with spring boot configuration

---
+++ academic-business/src/main/java/com/college/academic/App.java
@@ -0,0 +1,13 @@
+package com.college.academic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
